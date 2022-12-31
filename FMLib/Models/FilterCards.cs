3830e7cdcd1b43101019646a8abd251d3d21c4f3
Yure Moreira Lima <yuri-lolz@hotmail.com>
2022-12-31 09:57:29 -0300

    Update some packages and little changes to compile de project and starting to create de starter deck and duelist deck filter.

---
+++ FMLib/Models/FilterCards.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMLib.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FilterCards
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<int> BannedId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MinimumAttack { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MaximumAttack { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MinimumDefense { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MaximumDefense { get; set; }
+    }
+}
